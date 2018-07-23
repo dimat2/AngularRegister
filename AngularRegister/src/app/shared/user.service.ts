f7153f507a81ddd78c59ac53359e43d1086f5d4b
dimat2 <40808548+dimat2@users.noreply.github.com>
2018-07-23 22:17:42 +0200

    Angular

---
+++ AngularRegister/src/app/shared/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { User } from './user.model';
+
+@Injectable()
+export class UserService {
+  readonly rootUrl = 'http://localhost:55042';
+  constructor(private http: HttpClient) { }
+
+  registerUser(user : User) {
+    const body: User = {
+      UserName: user.UserName,
+      Password: user.Password,
+      Email: user.Email,
+      FirstName: user.FirstName,
+      LastName: user.LastName
+    }
+    return this.http.post(this.rootUrl + '/api/User/Register', body);
+  }
+}
